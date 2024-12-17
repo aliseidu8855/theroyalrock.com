edce38dd12b71691559286725c4c937b04095b3e
aliseidu8855 <aliseidu8855@gmail.com>
2024-12-17 17:19:07 +0000

    refactor Gallery component; update CSS class names, improve layout, and add a button for more photos

--- theroyalrock_frontend/src/components/Gallery/Gallery.jsx
+++ theroyalrock_frontend/src/components/Gallery/Gallery.jsx
@@ -3,38 +3,43 @@ import './Gallery.css'
 import photo1 from "../../assets/images/gallery/photo1.jpg"
 import photo2 from "../../assets/images/gallery/photo2.jpg"
 import photo3 from "../../assets/images/gallery/photo3.jpg"
+import photo4 from "../../assets/images/gallery/photo4.jpg"
 
 const Gallery = () => {
     return (
-        <div className='gallery-photos'>
-            <div className='gallery-photo'>
-                <img src={photo1} alt='photo1' />
-                <div className='caption'>
-                    <span class="material-symbols-outlined">
-                        arrow_forward
-                    </span>
-                    <p>View More</p>
+        <div className='gallery-container'>
+            <div className='gallery-photos'>
+                <div className='gallery-photo'>
+                    <img src={photo1} alt='photo1' />
+                    <div className='caption'>
+                        <span class="material-symbols-outlined">
+                            arrow_forward
+                        </span>
+                        <p>View More</p>
+                    </div>
+                </div>
+                <div className='gallery-photo'>
+                    <img src={photo2} alt='photo2' />
+                    <div className='caption'>
+                        <span class="material-symbols-outlined">
+                            arrow_forward
+                        </span>
+                        <p>View More</p>
+                    </div>
+                </div>
+                <div className='gallery-photo'>
+                    <img src={photo3} alt='photo3' />
+                    <div className='caption'>
+                        <span class="material-symbols-outlined">
+                            arrow_forward
+                        </span>
+                        <p>View More</p>
+                    </div>
                 </div>
             </div>
-            <div className='gallery-photo'>
-                <img src={photo2} alt='photo2' />
-                <div className='caption'>
-                    <span class="material-symbols-outlined">
-                        arrow_forward
-                    </span>
-                    <p>View More</p>
-                </div>
-            </div>
-            <div className='gallery-photo'>
-                <img src={photo3} alt='photo3' />
-                <div className='caption'>
-                    <span class="material-symbols-outlined">
-                        arrow_forward
-                    </span>
-                    <p>View More</p>
-                </div>
-            </div>
-
+            <button className='btn dark-btn'> See more here <span class="material-symbols-outlined">
+                arrow_forward
+            </span></button>
         </div>
     )
 }
